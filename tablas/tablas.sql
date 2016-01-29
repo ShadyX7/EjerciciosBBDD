f19b928e9ba1968f94c9b95b29cf24d067286f09
Victor <Victor>
2016-01-30 00:16:26 +0100

    Ejercicios de https://www.aceptaelreto.com/

---
+++ tablas/tablas.sql
@@ -0,0 +1,13 @@
+start c:/tablas/tablasuni3.sql;
+start c:/tablas/tablasuni4.sql;
+start c:/tablas/tablasuni5.sql;
+start c:/tablas/tablasuni8.sql;
+start c:/tablas/tablasuni9.sql;
+start c:/tablas/tablasuni10.sql;
+start c:/tablas/tablasuni11.sql;
+start c:/tablas/tablasuni12.sql;
+start c:/tablas/tablasuni13.sql;
+start c:/tablas/tablasuni14.sql;
+start c:/tablas/tablasuni15.sql;
+start c:/tablas/tablasuni6.sql;
+
